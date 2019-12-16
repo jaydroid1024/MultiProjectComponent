75f6ab9b0f92dcc07b2779c2ff04cfc665abee16
王学杰 <xuejie.wang@cityio.cn>
2019-12-16 17:32:26 +0800

    添加公共业务组件component_base

---
+++ Components/component_base/src/main/java/com/jaydroid/conponent_base/DataManager.kt
@@ -0,0 +1,16 @@
+package com.jaydroid.conponent_base
+
+/**
+ * 数据管理类
+ * @author wangxuejie
+ * @date 2019-12-16 16:41
+ * @version 1.0
+ */
+object DataManager {
+    /**
+     * 获取登录信息
+     */
+    fun loginRequest(): String {
+        return "Jay"
+    }
+}
--- Components/component_login/src/main/java/com/jaydroid/component_login/LoginActivity.kt
+++ Components/component_login/src/main/java/com/jaydroid/component_login/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.jaydroid.component_login
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.jaydroid.conponent_base.DataManager
 import kotlinx.android.synthetic.main.activity_login.*
 
 @Route(path = "/component_login/path/login/activity")
@@ -12,6 +13,8 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         tv_from.text = intent.getStringExtra("from")
+
+        tv_from.append("\n 登录人信息:" + DataManager.loginRequest())
 
     }
 }
